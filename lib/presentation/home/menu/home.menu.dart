c344be0a2be3c31dcdd19a9a6b5d2d630cf119ee
Indra Kusuma <146909443+Indrakusuma007@users.noreply.github.com>
2025-06-04 14:24:51 +0700

    p

--- lib/presentation/home/menu/home.menu.dart
+++ lib/presentation/home/menu/home.menu.dart
@@ -40,7 +40,7 @@ class HomeMenu extends GetView<HomeController> {
                 onTap: () => Get.toNamed(Routes.PELAYANAN),
               ),
               HomeMenuItem(
-                title: 'Prol',
+                title: 'Profil',
                 icon: Icons.person_2_sharp,
                 onTap: () => Get.toNamed(Routes.PROFILE),
               ),
